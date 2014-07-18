12b16b6ca0ceb2859cc4140ca889dfa1bc67f007
britg <brit@britg.com>
2014-07-17 20:59:18 -0500

    Dev assets

--- Assets/Triforce/Scripts/Camera/FollowPlayerController.cs
+++ Assets/Triforce/Scripts/Camera/FollowPlayerController.cs
@@ -7,7 +7,7 @@ public class FollowPlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+    Application.targetFrameRate = 60;
 	}
 	
 	// Update is called once per frame
